abc4b6315ce808e918fc0e9ad458bfe25b1180dc
User <User@WIN-FHSUTN5TG5D>
2020-03-25 16:24:37 +0600

    Добавьте файлы проекта.

---
+++ dz11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher();
+            teacher.Print();
+            Student student = new Student();
+            student.Print();
+            student.Calculate();
+            Person person = new Person();
+            person.Print();
+
+
+            Console.ReadLine();
+        }
+    }
+}
